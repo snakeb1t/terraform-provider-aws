edcf351f72f592f9195ed1d48320ccfb55af93e1
Paul Tittle <ptittle@palantir.com>
2019-02-27 13:30:15 -0500

    can now specify acl type on bucket import

--- aws/import_aws_s3_bucket.go
+++ aws/import_aws_s3_bucket.go
@@ -2,6 +2,7 @@ package aws
 
 import (
 	"github.com/hashicorp/terraform/helper/schema"
+	"strings"
 )
 
 func resourceAwsS3BucketImportState(
@@ -9,8 +10,15 @@ func resourceAwsS3BucketImportState(
 	meta interface{}) ([]*schema.ResourceData, error) {
 
 	results := make([]*schema.ResourceData, 1)
+	idData := strings.Split(d.Id(), "_")
+
 	d.Set("force_destroy", false)
-	d.Set("acl", "private")
+	if len(idData) == 2 {
+		d.Set("acl", idData[1])
+	} else {
+		d.Set("acl", "private")
+	}
+	d.SetId(idData[0])
 	results[0] = d
 
 	return results, nil
